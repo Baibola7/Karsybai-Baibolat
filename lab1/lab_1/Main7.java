cdac3ddc452898c5ba7aea4362d3ba0e1c909e0f
Lenovo <qarsybaibaibolat@gmail.com>
2023-04-09 22:13:46 +0600

    Method to find binomial coefficient

---
+++ lab1/lab_1/Main7.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main7 {
+    /*
+        @getReverse - get reversed list
+        @param1 - integer array (array)
+        @param2 - integer number (start) reverse start index
+        @param3 - integer number (end) reverse end index
+        @return - reversed array
+    */
+    public static void getReverse(int[] arr, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        getReverse(arr, start+1, end-1);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] array = new int[N];
+        for (int i = 0; i < N; i++){
+            array[i] = scanner.nextInt();
+        }
+        getReverse(array, 0, N-1);
+        for (int i : array){
+            System.out.print(i + " ");
+        }
+    }
+}
