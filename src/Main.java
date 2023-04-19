2df509fc549ac1f3d78253ecf48a53336d4ef2aa
Lenovo <qarsybaibaibolat@gmail.com>
2023-04-19 22:09:01 +0600

    submit method MyLinkedList

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        MyArrayList myArrayList = new MyArrayList();
+//        myArrayList.add();
+//        myArrayList.add();
+//        myArrayList.sort();
+//        myArrayList.remove();
+//        myArrayList.remove();
+//        myArrayList.clear();
+//        myArrayList.get();
+//        myArrayList.contains();
+//        myArrayList.indexOf();
+//        myArrayList.lastIndexOf();
+//        myArrayList.size();
+        myArrayList.add(100);
+        myArrayList.add(10);
+        myArrayList.add(1230);
+        myArrayList.sort();
+        System.out.println(myArrayList.get(0));
+        System.out.println(myArrayList.get(1));
+        System.out.println(myArrayList.get(2));
+    }
+}
---
+++ src/MyArrayList.java
@@ -0,0 +1,123 @@
+public class MyArrayList implements MyList {
+    private Object[] arr;
+    private int size;
+
+    MyArrayList() {
+        this.size = 0;
+        this.arr = new Object[5];
+    }
+
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+
+    @Override
+    public boolean contains(Object o) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == o) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void increaseBuffer() {
+        Object[] newArr = new Object[arr.length * 2];
+        for (int i = 0; i < arr.length; i++) {
+            newArr[i] = arr[i];
+        }
+        arr = newArr;
+    }
+
+    @Override
+    public void add(Object item) {
+        if (size == arr.length) {
+            increaseBuffer();
+        }
+        else {
+            arr[size++] = item;
+        }
+    }
+
+    @Override
+    public void add(Object item, int index) {
+        if (index >= arr.length) {
+            increaseBuffer();
+        } else {
+            arr[index] = item;
+        }
+        size++;
+    }
+
+    @Override
+    public boolean remove(Object item) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].toString().equals(item.toString())) {
+                System.out.println(remove(i));
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Object remove(int index){
+        for(; index < size; index++){
+            if(index < arr.length - 1){
+                arr[index] = arr[index + 1];
+            }
+        }
+        size--;
+        return "Successfully";
+    }
+
+    @Override
+    public void clear(){
+        this.arr = new Object[5];
+        this.size = 0;
+    }
+    @Override
+    public Object get(int index) {
+        if (index < arr.length) {
+            return arr[index];
+        }
+        else{
+            return "Index not found";
+        }
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i].toString().equals(o.toString())){
+                return i;
+            }
+        }
+        return 1;
+    }
+    @Override
+    public int lastIndexOf(Object o) {
+        for (int i = arr.length - 1; i >= 0;  i--){
+            if (arr[i].toString().equals(o.toString())){
+                return i;
+            }
+        }
+        return 1;
+    }
+
+    @Override
+    public void sort() {
+        for (int i = 0; i < size; i++){
+            for (int j = i; j < size; j++){
+                if ((int)arr[j] < (int) arr[i]){
+                    Object item = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = item;
+                }
+            }
+        }
+    }
+}
